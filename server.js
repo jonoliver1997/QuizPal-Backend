634991a37f7f899e2717379cc4084577e934141c
Jonathan Oliver <jonoliver1997@gmail.com>
2023-11-22 09:51:05 -0800

    initial server setup

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+require("dotenv").config();
+const port = process.env.PORT;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}...`);
+});
