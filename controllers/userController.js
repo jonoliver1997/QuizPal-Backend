2e922a82cde9c139e40c7032759e5e0fa2b1383a
Jonathan Oliver <jonoliver1997@gmail.com>
2023-12-03 09:59:02 -0800

    convert us_id back into objectID

--- controllers/userController.js
+++ controllers/userController.js
@@ -80,13 +80,9 @@ const loginUser = asyncHandler(async (req, res) => {
 
   // Generate a JSON Web Token (JWT)
   console.log(user._id.toString());
-  const token = jwt.sign(
-    { userId: user._id.toString() },
-    process.env.jwtSecret,
-    {
-      expiresIn: "1h",
-    }
-  );
+  const token = jwt.sign({ userId: user._id }, process.env.jwtSecret, {
+    expiresIn: "1h",
+  });
   console.log("Generated JWT Token", token);
   console.log("User ID: ", token.userId);
 
