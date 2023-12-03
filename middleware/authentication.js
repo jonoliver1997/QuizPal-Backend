c2660ac82640061e9ae8eac8db5656bb0c614a5b
Jonathan Oliver <jonoliver1997@gmail.com>
2023-12-03 09:18:39 -0800

    enhance error handling in authentication middlewar

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -13,15 +13,19 @@ const options = {
 passport.use(
   new JwtStrategy(options, async (jwtPayload, done) => {
     try {
+      console.log("jwtPayload: ", jwtPayload);
       // Find the user based on the ID in jwtPayload
       const user = await User.findById(jwtPayload.id);
 
       if (user) {
+        console.log("User found: ", user);
         return done(null, user);
       } else {
+        console.log("User not found");
         return done(null, false);
       }
     } catch (error) {
+      console.log("Error in passport.use:");
       return done(error, false);
     }
   })
