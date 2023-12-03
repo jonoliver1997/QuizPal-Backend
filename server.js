78ff2085d03ec79e56045b1a4521c5bb24e333da
Jonathan Oliver <jonoliver1997@gmail.com>
2023-12-03 07:49:54 -0800

    fix cors issue

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
 const passport = require("passport");
+const cors = require("cors");
 
 const deckRoutes = require("./routes/deckRoutes");
 const userRoutes = require("./routes/userRoutes");
@@ -14,6 +15,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
 
 app.use(passport.initialize());
 
